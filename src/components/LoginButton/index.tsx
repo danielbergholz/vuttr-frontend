352f6242753f5a9e9c254e437ff13ccc5b2625e5
Daniel Bergholz <bergholz.daniel@gmail.com>
2020-08-05 19:41:27 -0300

    fix button html attributes

--- src/components/LoginButton/index.tsx
+++ src/components/LoginButton/index.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { ButtonHTMLAttributes } from 'react';
 
 import { Container } from './styles';
 
-interface ButtonProps {
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   color?: string;
   hover?: string;
   loading?: number;
